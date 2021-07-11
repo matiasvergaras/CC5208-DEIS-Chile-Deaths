cd78ce93802444e6e91adf8774a9b09c1cc0ae0b
Fabs <fabianosses@ug.uchile.cl>
2021-07-11 19:59:27 -0400

    añadi selector por año, no cambie el color xd

--- Pagina/vis3/vis3.js
+++ Pagina/vis3/vis3.js
@@ -1,14 +1,18 @@
 export default function define(runtime, observer) {
   const main = runtime.module();
-  const fileAttachments = new Map([["formated-data per month.csv",new URL("./formated-data per month.csv",import.meta.url)],["formated-data-men.csv",new URL("./formated-data-men.csv",import.meta.url)],["formated-data-women.csv",new URL("./formated-data-women.csv",import.meta.url)]]);
+  const fileAttachments = new Map([["formated-data.csv",new URL("./data/formated-data.csv",import.meta.url)],["formated-data-men.csv",new URL("./data/formated-data-men.csv",import.meta.url)],["formated-data-women.csv",new URL("./data/formated-data-women.csv",import.meta.url)],["formated-data16.csv",new URL("./data/formated-data16.csv",import.meta.url)],["formated-data16m.csv",new URL("./data/formated-data16m.csv",import.meta.url)],["formated-data16w.csv",new URL("./data/formated-data16w.csv",import.meta.url)],["formated-data17.csv",new URL("./data/formated-data17.csv",import.meta.url)],["formated-data17m.csv",new URL("./data/formated-data17m.csv",import.meta.url)],["formated-data17w.csv",new URL("./data/formated-data17w.csv",import.meta.url)],["formated-data18.csv",new URL("./data/formated-data18.csv",import.meta.url)],["formated-data18m.csv",new URL("./data/formated-data18m.csv",import.meta.url)],["formated-data18w.csv",new URL("./data/formated-data18w.csv",import.meta.url)],["formated-data19.csv",new URL("./data/formated-data19.csv",import.meta.url)],["formated-data19m.csv",new URL("./data/formated-data19m.csv",import.meta.url)],["formated-data19w.csv",new URL("./data/formated-data19w.csv",import.meta.url)],["formated-data20.csv",new URL("./data/formated-data20.csv",import.meta.url)],["formated-data20m.csv",new URL("./data/formated-data20m.csv",import.meta.url)],["formated-data20w.csv",new URL("./data/formated-data20w.csv",import.meta.url)],["formated-data21.csv",new URL("./data/formated-data21.csv",import.meta.url)],["formated-data21m.csv",new URL("./data/formated-data21m.csv",import.meta.url)],["formated-data21w.csv",new URL("./data/formated-data21w.csv",import.meta.url)]]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
-  main.variable(observer("taitol")).define(["md"], function(md){return(
+  main.variable(observer()).define(["md"], function(md){return(
 md`# Evolucion enfermedades respiratorias por mes según grupo etario`
 )});
   main.variable(observer("viewof selD")).define("viewof selD", ["Inputs","dataSel"], function(Inputs,dataSel){return(
-Inputs.select(dataSel, {label: "selector", format: x => x[0], value: dataSel.find(x => x[0] === "general")})
+Inputs.select(dataSel, {label: "por genero", format: x => x[0], value: dataSel.find(x => x[0] === "general")})
 )});
   main.variable(observer("selD")).define("selD", ["Generators", "viewof selD"], (G, _) => G.input(_));
+  main.variable(observer("viewof gy")).define("viewof gy", ["Inputs","years"], function(Inputs,years){return(
+Inputs.select(years, {label: "por año", format: x => x[0], value: years.find(x => x[0] === "general")})
+)});
+  main.variable(observer("gy")).define("gy", ["Generators", "viewof gy"], (G, _) => G.input(_));
   main.variable(observer("matrix")).define("matrix", ["d3","DOM","width","height","x","y","selD","categories","color","xAxis","yAxis"], function(d3,DOM,width,height,x,y,selD,categories,color,xAxis,yAxis)
 {
   const svg = d3.select(DOM.svg(width, height))
@@ -22,8 +26,8 @@ Inputs.select(dataSel, {label: "selector", format: x => x[0], value: dataSel.fin
   const tooltip = d3.select("body").append("div")
     .style("position","absolute")
     .style("display","block")
-    .style("background","gray")
-    .style("min-width","10px")
+    .style("background","white")
+    .style("min-width","100px")
     .style("max-width","440px")
     .style("border","1px solid white")
     .style("border-radius","4px") 
@@ -91,13 +95,10 @@ Inputs.select(dataSel, {label: "selector", format: x => x[0], value: dataSel.fin
   return svg.node();
 }
 );
-  main.variable(observer()).define(["color"], function(color){return(
-color(57)
-)});
   main.variable(observer("color")).define("color", ["d3","max_val"], function(d3,max_val){return(
 d3.scaleLinear()
-  .domain([0, max_val])
-  .range(["pink", "red"])
+    .domain([0, max_val/2,max_val])
+    .range(d3.schemeOrRd[3])
 )});
   main.variable(observer("x")).define("x", ["d3","age","margin","width"], function(d3,age,margin,width){return(
 d3.scaleBand()
@@ -141,37 +142,28 @@ data.columns.splice(1)
 selD[1].map((d) => d.index)
 )});
   main.variable(observer("dataSel")).define("dataSel", ["data","data_men","data_women"], function(data,data_men,data_women){return(
-[
-  ["general", data],
-  ["hombres", data_men],         
-  ["mujeres", data_women]]
+[["general", data], ["hombres", data_men], ["mujeres",data_women]]
 )});
-  main.variable(observer("data_women")).define("data_women", ["d3","file_t_w"], function(d3,file_t_w){return(
-d3.csvParse(file_t_w)
+  main.variable(observer("data_women")).define("data_women", ["d3","FileSel","gy"], async function(d3,FileSel,gy){return(
+d3.csvParse(await FileSel[gy[1]][1][2].text())
 )});
-  main.variable(observer("data_men")).define("data_men", ["d3","file_t_m"], function(d3,file_t_m){return(
-d3.csvParse(file_t_m)
+  main.variable(observer("data_men")).define("data_men", ["d3","FileSel","gy"], async function(d3,FileSel,gy){return(
+d3.csvParse(await FileSel[gy[1]][1][1].text())
 )});
-  main.variable(observer("data")).define("data", ["d3","file_t"], function(d3,file_t){return(
-d3.csvParse(file_t)
+  main.variable(observer("data")).define("data", ["d3","FileSel","gy"], async function(d3,FileSel,gy){return(
+d3.csvParse(await FileSel[gy[1]][1][0].text())
 )});
-  main.variable(observer("file_t_w")).define("file_t_w", ["file_women"], function(file_women){return(
-file_women.text()
+  main.variable(observer("FileSel")).define("FileSel", ["FileAttachment"], function(FileAttachment){return(
+[["general", [FileAttachment("formated-data.csv"),    FileAttachment("formated-data-men.csv"), FileAttachment("formated-data-women.csv")]],
+           ["2016", [FileAttachment("formated-data16.csv"), FileAttachment("formated-data16m.csv"), FileAttachment("formated-data16w.csv")]],
+           ["2017", [FileAttachment("formated-data17.csv"), FileAttachment("formated-data17m.csv"), FileAttachment("formated-data17w.csv")]],
+           ["2018", [FileAttachment("formated-data18.csv"), FileAttachment("formated-data18m.csv"), FileAttachment("formated-data18w.csv")]],
+           ["2019", [FileAttachment("formated-data19.csv"), FileAttachment("formated-data19m.csv"), FileAttachment("formated-data19w.csv")]],
+           ["2020", [FileAttachment("formated-data20.csv"), FileAttachment("formated-data20m.csv"), FileAttachment("formated-data20w.csv")]],
+           ["2021", [FileAttachment("formated-data21.csv"), FileAttachment("formated-data21m.csv"), FileAttachment("formated-data21w.csv")]]]
 )});
-  main.variable(observer("file_t_m")).define("file_t_m", ["file_men"], function(file_men){return(
-file_men.text()
-)});
-  main.variable(observer("file_t")).define("file_t", ["file"], function(file){return(
-file.text()
-)});
-  main.variable(observer("file_women")).define("file_women", ["FileAttachment"], function(FileAttachment){return(
-FileAttachment("formated-data-women.csv")
-)});
-  main.variable(observer("file_men")).define("file_men", ["FileAttachment"], function(FileAttachment){return(
-FileAttachment("formated-data-men.csv")
-)});
-  main.variable(observer("file")).define("file", ["FileAttachment"], function(FileAttachment){return(
-FileAttachment("formated-data per month.csv")
+  main.variable(observer("years")).define("years", function(){return(
+[["todos los años",0],["2016",1],["2017",2],["2018",3],["2019",4],["2020",5],["2021",6]]
 )});
   main.variable(observer("height")).define("height", ["width"], function(width){return(
 width
